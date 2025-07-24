735b3c446fc3709478b58f9ff1a97f553ca24a51
Fafner <alex2000926@naver.com>
2025-07-24 16:10:15 +0900

    update

--- DateExample.java
+++ DateExample.java
@@ -8,7 +8,7 @@ public class DateExample { // 클래스 정의 시작
 
 
         //오후 3시 30분 출력 연습
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a hh:mm:ss"); // 값 대입
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 M월 dd일 a h시 m분"); // 값 대입
         String formatted = sdf.format(now); // 변수 선언 또는 초기화
         System.out.println("현재 날짜와 시간: " + formatted); // 콘솔에 메시지 출력
     } // 코드 블록 종료
